a1fb5f0d48e9ee0f8af662f3aa9e318b5311f5cd
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-11 11:42:33 -0400

    Add basic accept_new_client

--- lib/server.rb
+++ lib/server.rb
@@ -1,3 +1,26 @@
+require 'socket'
+
 class Server
-  
+  def port_number
+    3000
+  end
+
+  def clients
+    @clients ||= []
+  end
+
+  def start
+    @server = TCPServer.new(port_number)
+  end
+
+  def stop
+    @server.close if @server
+  end
+
+  def accept_new_client(player_name)
+    client = @server.accept_nonblock
+    clients << client
+    client.puts "Welcome!"
+  rescue IO::WaitReadable, Errno::EINTR
+  end
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,3 +1,61 @@
+require 'server'
+
+class MockFishSocketClient
+  attr_reader :socket, :output
+
+  def initialize(port)
+    @socket = TCPSocket.new('localhost', port)
+  end
+
+  def provide_input(text)
+    @socket.puts(text)
+  end
+
+  def capture_output(delay = 0.1)
+    sleep(delay)
+    @output = @socket.read_nonblock(1000)
+  rescue IO::WaitReadable
+    @output = ''
+  end
+
+  def close
+    @socket.close if @socket
+  end
+end
+
 describe Server do
-  
+  before(:each) do
+    @clients = []
+    @server = Server.new
+    @server.start
+    sleep 0.1
+  end
+
+  after(:each) do
+    @server.stop
+    @clients.each do |client|
+      client.close
+    end
+  end
+
+  it 'has clients' do
+    expect(@server).to respond_to :clients
+  end
+
+  it 'has a port_number' do
+    expect(@server).to respond_to :port_number
+  end
+
+  describe '#accept_new_client' do
+    let(:client1) { MockFishSocketClient.new(@server.port_number) }
+
+    before do
+      @clients.push(client1)
+    end
+    
+    it 'sends a welcome message to client' do
+      @server.accept_new_client('Player 1')
+      expect(client1.capture_output).to match(/welcome/i)
+    end
+  end
 end
