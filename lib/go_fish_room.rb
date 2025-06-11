9a14132a81333ead4ba62018b729fa0c84621c84
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-11 13:39:19 -0400

    Add create_game
    Co-authored-by: Oliver Stevens <OliverwStevens@users.noreply.github.com>

--- lib/go_fish_room.rb
+++ lib/go_fish_room.rb
@@ -1,8 +1,15 @@
+require 'go_fish_game'
+
 class GoFishRoom
-  attr_reader :game, :clients
+  attr_reader :clients
+  attr_accessor :game
   
   def initialize(clients)
     @game = nil
     @clients = clients
+  end
+
+  def create_game
+    self.game = GoFishGame.new
   end
 end
--- spec/go_fish_room_spec.rb
+++ spec/go_fish_room_spec.rb
@@ -1,12 +1,42 @@
 require 'go_fish_room'
+require 'mock_fish_socket_client'
+require 'server'
 
 describe GoFishRoom do
-  let(:room) { GoFishRoom.new }
+  let(:client1) { MockFishSocketClient.new(@server.port_number) }
+  let(:client2) { MockFishSocketClient.new(@server.port_number) }
+
+  before(:each) do
+    @clients = []
+    @server = Server.new
+    @server.start
+    sleep 0.1
+    @clients.push(client1)
+    @server.accept_new_client('Player 1')
+    @clients.push(client2)
+    @server.accept_new_client('Player 2')
+  end
+
+  after(:each) do
+    @server.stop
+    @clients.each do |client|
+      client.close
+    end
+  end
+
+  let(:room) { GoFishRoom.new([client1, client2]) }
   it 'has a game' do
     expect(room).to respond_to :game
   end
 
   it 'has clients' do
     expect(room).to respond_to :clients
+  end
+
+  describe '#create_game' do
+    it 'sets game to GoFishGame' do
+      room.create_game
+      expect(room.game).to respond_to :deck
+    end
   end
 end
