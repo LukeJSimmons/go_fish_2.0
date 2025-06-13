47e53fc7a0b48d25a72936fee539e44302270b93
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-13 08:50:43 -0400

    Add display_result_message with basic output functions

--- lib/round_result.rb
+++ lib/round_result.rb
@@ -8,4 +8,25 @@ class RoundResult
     @matching_cards = matching_cards
     @fished_card = fished_card
   end
+
+  def display_result_message_to(player)
+    return target_has_request(player) unless matching_cards.empty?
+    return target_does_not_have_request(player)
+  end
+
+  private
+
+  def target_has_request(player)
+    return "You took #{matching_cards.count} #{card_request}#{multiple_cards?} #{target.name}" if player == :current_player
+    "#{current_player.name} took #{matching_cards.count} #{card_request}#{multiple_cards?} #{target.name}"
+  end
+
+  def target_does_not_have_request(player)
+    return "You requested #{card_request} from #{target.name}, but took nothing\nGo fish, you drew a #{fished_card.rank}" if player == :current_player
+    "#{current_player.name} requested #{card_request} from #{target.name}, but took nothing"
+  end
+
+  def multiple_cards?
+    's' if matching_cards.count > 1
+  end
 end
--- spec/round_result_spec.rb
+++ spec/round_result_spec.rb
@@ -1,5 +1,106 @@
 require 'round_result'
+require 'go_fish_player'
+require 'card'
 
 describe RoundResult do
-  
+  describe '#display_result_message_to' do
+    context 'when target has requested card' do
+      let(:result) { RoundResult.new(
+        current_player: GoFishPlayer.new('Player 1'),
+        target: GoFishPlayer.new('Player 2'),
+        card_request: 'A',
+        matching_cards: [Card.new('A','C')],
+        fished_card: nil
+        )
+      }
+
+      it 'displays target' do
+        expect(result.display_result_message_to(:current_player)).to include result.target.name
+      end
+
+      it 'displays card_request' do
+        expect(result.display_result_message_to(:current_player)).to include result.card_request
+      end
+
+      it 'displays how many cards you took' do
+        expect(result.display_result_message_to(:current_player)).to include "#{result.matching_cards.count}"
+      end
+
+      context 'when target has multiple matching cards' do
+        let(:result) { RoundResult.new(
+          current_player: GoFishPlayer.new('Player 1'),
+          target: GoFishPlayer.new('Player 2'),
+          card_request: 'A',
+          matching_cards: [Card.new('A','C'),Card.new('A','S')],
+          fished_card: nil
+          )
+        }
+        
+        it 'displays request in plural' do
+          expect(result.display_result_message_to(:current_player)).to include "#{result.card_request}s"
+        end
+      end
+
+      context 'when displaying to current_player' do
+        it 'displays in the 2nd person' do
+          expect(result.display_result_message_to(:current_player)).to match (/you/i)
+        end
+      end
+
+      context 'when not displaying to current_player' do
+        it 'displays in the 3rd person' do
+          expect(result.display_result_message_to(:opponents)).to include "#{result.current_player.name}"
+        end
+      end
+    end
+
+    context 'when target does not have requested card' do
+      context 'when fished card is not requested card' do
+        let(:result) { RoundResult.new(
+          current_player: GoFishPlayer.new('Player 1'),
+          target: GoFishPlayer.new('Player 2'),
+          card_request: 'A',
+          matching_cards: [],
+          fished_card: Card.new('2','H')
+          )
+        }
+
+        it 'displays target' do
+          expect(result.display_result_message_to(:current_player)).to include result.target.name
+        end
+
+        it 'displays card_request' do
+          expect(result.display_result_message_to(:current_player)).to include result.card_request
+        end
+
+        it 'displays that the target did not have the card request' do
+          expect(result.display_result_message_to(:current_player)).to match (/nothing/i)
+        end
+
+        it 'displays go fish' do
+          expect(result.display_result_message_to(:current_player)).to match (/go fish/i)
+        end
+
+        context 'when displaying to current_player' do
+          it 'displays in the 2nd person' do
+            expect(result.display_result_message_to(:current_player)).to match (/you/i)
+          end
+
+          it 'displays fished card' do
+            expect(result.display_result_message_to(:current_player)).to include "drew a #{result.fished_card.rank}"
+          end
+        end
+
+        context 'when not displaying to current_player' do
+          it 'displays in the 3rd person' do
+            expect(result.display_result_message_to(:opponents)).to include "#{result.current_player.name}"
+          end
+
+          it 'does not display drawn card' do
+            expect(result.display_result_message_to(:opponents)).to_not include "drew a #{result.fished_card.rank}"
+          end
+        end
+      end
+    end
+  end
 end
